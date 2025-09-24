9c010d89f4a38b78da7b14c55c5f4147f9f2b829
xz925401102 <xz925401102@gmail.com>
2025-09-24 22:24:03 +0100

    Add files via upload

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Thanks for watching!",
+    "title": "后疫情的回响 Echoes of the Post-Pandemic",
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "auto",
+    "description": "",
+    "thumbnail": "Medias/Photos/1.jpg",
+    "noWording": "No",
+    "url": "",
+    "yesWording": "Yes",
+    "launchAppWording": "Then Launch Project",
+    "downloadAppWording": "Download App",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "analyticsKey": ""
+}
